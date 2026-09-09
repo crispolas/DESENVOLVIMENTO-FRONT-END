4961dc97c5bd7090c06db09759bd2dfb2d6cbf8c
Crispim <108757653+crispolas@users.noreply.github.com>
2026-09-09 20:15:53 -0300

    Busca do arquivo, verificação de sucesso, leitura e captura dos dados

--- js/api.js
+++ js/api.js
@@ -1,2 +1,11 @@
 export async function carregarTarefas() {
+  const resposta = await fetch("./dados.json");
+  
+  if (!resposta.ok) {
+  throw new Error(`Erro HTTP: ${resposta.status}`);
+  }
+
+  const dados = await resposta.json();
+  return dados.tarefas;
 }
+
