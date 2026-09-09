c46055f5ea1b66d5030c90dbcb2e05881935eb00
Crispim <108757653+crispolas@users.noreply.github.com>
2026-09-09 20:20:16 -0300

    Cria a função que vai ler e verificar o estado

--- js/estados.js
+++ js/estados.js
@@ -1,1 +1,15 @@
+export function renderizarEstado(estado, dados) {
+  switch (estado) {
+    case "carregando":
+      break;
 
+    case "sucesso":
+      break;
+
+    case "vazio":
+      break;
+
+    case "erro":
+      break;
+  }
+}
